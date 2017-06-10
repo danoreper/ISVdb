2cd7cc2efe879349f6aa5c86395d7fb05bf39700
Dan Oreper <doreper@killdevil-login2.kd.unc.edu>
2017-06-10 19:23:01 -0400

    a

--- install.sh
+++ install.sh
@@ -1,14 +1,19 @@
-# echo "Copying files from Zenodo"
-# curl https://zenodo.org/record/399474/files/File_S2.tar.gz > ./data/cc_probs_2016-03_mm10.tar.gz
-# cd ./data/
-# tar -xzvf cc_probs_2016-03_mm10.tar.gz
-# cd -
+echo "Copying files from Zenodo"
+curl https://zenodo.org/record/399474/files/File_S2.tar.gz > ./data/cc_probs_2016-03_mm10.tar.gz
+cd ./data/
+tar -xzvf cc_probs_2016-03_mm10.tar.gz
+cd -
 
-# curl  https://zenodo.org/record/399474/files/File_S3.gz > ./data/vcf/rel1410/mgp.v4.snps.dbSNP.vcf.gz
+curl  https://zenodo.org/record/399474/files/File_S3.gz > ./data/vcf/rel1410/mgp.v4.snps.dbSNP.vcf.gz
 
-# curl  https://zenodo.org/record/399474/files/File_S4.gz > ./data/vcf/rel1410/mgp.v4.indels.dbSNP.vcf.gz
+curl  https://zenodo.org/record/399474/files/File_S4.gz > ./data/vcf/rel1410/mgp.v4.indels.dbSNP.vcf.gz
 
-# curl  https://zenodo.org/record/399474/files/File_S5.gz > ./data/vcf/rel1410/mgp.v4.snps.MT.dbSNP.vcf.gz
+curl  https://zenodo.org/record/399474/files/File_S5.gz > ./data/vcf/rel1410/mgp.v4.snps.MT.dbSNP.vcf.gz
+
+##curl ftp://ftp-mouse.sanger.ac.uk/REL-1505-SNPs_Indels/mgp.v5.merged.snps_all.dbSNP142.vcf.gz -O > mgp.v5.merged.snps_all.dbSNP142.vcf.gz
+
+##curl ftp://ftp-mouse.sanger.ac.uk/REL-1505-SNPs_Indels/mgp.v5.merged.indels.dbSNP142.normed.vcf.gz -O > mgp.v5.merged.indels.dbSNP142.normed.vcf.gz           
+
 
 mkdir ./data/b6_reference/mus_musculus.GRCm38.75/
 curl https://zenodo.org/record/399474/files/File_S7.gtf.tar.gz > ./data/b6_reference/mus_musculus.GRCm38.75/Mus_musculus.GRCm38.75.gtf.tar.gz
